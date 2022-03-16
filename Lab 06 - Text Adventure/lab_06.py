15fac36ae556c59e30cd2ee9d7aec5fa926b4a9e
Stefan Beadles <stefanbeadles@gmail.com>
2022-03-16 17:23:02 -0600

    Work on Lab 6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -3,26 +3,35 @@ while True:
 
     if answer.lower().strip() == "yes":
 
-        answer = input("You wake up in a dark room there are two rooms would you like to go north or east.\n Where would you like to go?").lower().strip()
+        answer = input(
+            "You wake up in a dark room\n There are two rooms would you like to go north or east.\n Where would you like to go?").lower().strip()
         if answer == "east":
-            answer = input("You have entered a very bright room, there are two other doors one leads north and the other goes east.\n Which one do you choose?")
+
+            answer = input(
+                "You have entered a very bright room,\n There are two other doors one leads north and the other goes east.\nWhich one do you choose?")
 
             if answer == "east":
                 print("You notice a massive creature you get eaten alive oh no!!.")
 
         if answer == "north":
-            answer == input("You have entered a highly decorated bedroom.\n There is a single door leading east go through?").lower().strip()
+
+            answer =input(
+                "You have entered a highly decorated bedroom.\n There is a single door leading east go through?").lower().strip()
             if answer == "east":
-                answer == input("This room has a single door leading north you wonder what it may lead to. Your body feels exhausted you better hurry!!")
-            elif answer == "north":
-                answer == input("You have discovered the alter of Gods and have gained total enlightenment!!\n You have completed the game congratulations hero")
+
+                answer =input("This room has a single door leading north you wonder what it may lead to. Your body feels exhausted you better hurry!!\n Will you go through the door?").lower().strip()
+
+                if answer == "north":
+                    answer =input("You have found a treasure trove of gold!")
             else:
-                print("Not an option you are killed")
-        elif answer == "south":
+                print("You have died")
+
+
+
+        if answer == "south":
             print("You walk into the wall and knock yourself out.")
         else:
             print("Not an option, you died!!")
 
     else:
         print("Maybe another time.")
-    break
